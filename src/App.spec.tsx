22984c3b549c61fb96f1d6c1a57510402c7bf84d
BraveVlad <giladvladpinker@gmail.com>
2024-06-06 04:01:30 +0300

    added use-event dep to simulate user actions instead of just firing events. added more specific query for heading role (level:1 means h1)

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -1,6 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
+import "@testing-library/user-event";
 
 import App from "./App";
 
@@ -8,7 +9,9 @@ describe("App", () => {
 	it("Renders welcome heading which mentions 'TDD' inside of it.", () => {
 		// ARRANGE - arrange the unitest
 		render(<App />);
-		const welcomeTitle = screen.getByRole("heading");
+		const welcomeTitle = screen.getByRole("heading", {
+			level: 1, // level means h1 (h1-6)}
+		});
 		// ACT - mock the user interaction
 
 		// EXCPECT - match the result with the expected outcome
