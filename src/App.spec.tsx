9ac4fdb2c1cfd17baba9835159fa1bbe4ff1d4dd
BraveVlad <giladvladpinker@gmail.com>
2024-06-06 06:12:14 +0300

    added routing tests to check whether bad router redirects NotFound with a title and a "back to home" button. Currently fails the back home button test.

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -2,13 +2,16 @@ import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import "@testing-library/user-event";
+import { MemoryRouter } from "react-router-dom";
 
-import App from "./App";
+import { AppContainer, App } from "./App";
 
 describe("App", () => {
+	const badRoute = "/bad/route/";
+
 	it("Renders welcome heading which mentions 'TDD' inside of it.", () => {
 		// ARRANGE - arrange the unitest
-		render(<App />);
+		render(<AppContainer />);
 		const welcomeTitle = screen.getByRole("heading", {
 			level: 1, // level means h1 (h1-6)}
 		});
@@ -16,5 +19,35 @@ describe("App", () => {
 
 		// EXCPECT - match the result with the expected outcome
 		expect(welcomeTitle).toHaveTextContent(/TDD/);
+	});
+
+	it("Renders NotFound if invalid path with a message", () => {
+		// ARRANGE
+		render(
+			<MemoryRouter initialEntries={[badRoute]}>
+				<App />
+			</MemoryRouter>
+		);
+		// ACT
+		// EXPECT
+		expect(screen.getByRole("heading", { level: 1 })).toHaveTextContent(
+			"Oopsie Doopsie!"
+		);
+	});
+
+	it("Renders NotFound if invalid path with a link to index", () => {
+		// ARRANGE
+
+		render(
+			<MemoryRouter initialEntries={[badRoute]}>
+				<App />
+			</MemoryRouter>
+		);
+		// ACT
+		// EXPECT
+		expect(screen.getByRole("link", { name: "Take me home!" })).toHaveAttribute(
+			"href",
+			"/test"
+		);
 	});
 });
